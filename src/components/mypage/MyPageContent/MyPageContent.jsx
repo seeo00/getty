8e893dc05391d07b680a245885319a27cd812ef6
hansol <gksthf9561@gmail.com>
2025-02-13 01:31:54 +0900

    반응형브레이킹포인트수정

--- src/components/mypage/MyPageContent/MyPageContent.jsx
+++ src/components/mypage/MyPageContent/MyPageContent.jsx
@@ -3,7 +3,7 @@ import PlanCard from '../PlanCard/PlanCard';
 import InfoNotes from '../InfoNotes/InfoNotes';
 import SubscriptionRow from '../SubscriptionRow/SubscriptionRow';
 import SubscriptionModal from '../SubscriptionModal/SubscriptionModal';
-import {
+import {   
   PageWrapper,
   ContentWrapper,
   TitleSection,
@@ -12,7 +12,7 @@ import {
   PlansGrid,
   ActionButton,
   MobileContentWrapper,
-  ButtonContainer,
+  ButtonContainer, 
 } from './style';
 import { InnerContainer } from '../../../common/layout/InnerContainer';
 
@@ -21,7 +21,7 @@ const MyPageContent = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handlePlanSelect = (plan) => {
-    setSelectedPlan(plan); // 단순히 새로운 플랜으로 설정, 해제 기능 없음
+    setSelectedPlan(plan);
   };
 
   const openModal = () => {
@@ -40,7 +40,7 @@ const MyPageContent = () => {
             <MainTitle>콘텐츠를 즐길 준비가 되셨나요?</MainTitle>
             <SubTitle>원하는 이용권을 선택해 주세요. 이용권은 언제든지 원하실 때 해지할 수 있습니다.</SubTitle>
           </TitleSection>
-
+           
           <PlansGrid>
             <ButtonContainer>
               <PlanCard title="Basic" selectedPlan={selectedPlan} onSelectPlan={handlePlanSelect} />
@@ -59,12 +59,11 @@ const MyPageContent = () => {
               </MobileContentWrapper>
             )}
           </PlansGrid>
-
+           
           <InfoNotes />
           <ActionButton onClick={openModal}>다음</ActionButton>
         </ContentWrapper>
       </PageWrapper>
-      {/* 모달 조건부 렌더링 */}
       {isModalOpen && <SubscriptionModal onClose={closeModal} />}
     </InnerContainer>
   );
