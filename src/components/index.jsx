bfdd81871c73efbbcfe09c93c44cd0b2e0aab368
seeo00 <csyoung0929@gmail.com>
2025-02-21 20:24:35 +0900

    오류 수정

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -14,7 +14,7 @@ export { default as RecTextList } from './main/RecTextList';
 export { default as SubBannerSwiper } from './category/SubBannerSwiper';
 export { default as CategoryButtons } from './category/CategoryButtons';
 export { default as CardContentList } from './category/CardContentList';
-export { default as ContentList } from './category/ContentList';
+// export { default as ContentList } from './category/ContentList';
 
 // mypage
 export { default as ProfilePage } from './mypage/profile/ProfilePage';
