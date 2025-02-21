2a6b436a5c3f361aa6791a05c5559e459f3e0ef5
seeo00 <csyoung0929@gmail.com>
2025-02-21 20:31:18 +0900

    오류 수정

--- src/components/category/CardContentList.jsx
+++ src/components/category/CardContentList.jsx
@@ -1,28 +1,28 @@
-// import CardBasic from '../../ui/card/CardBasic';
-// import { CardBasicSkeleton } from '../../ui/LoadingSkeleton';
-// import LoadingSpinner from '../../ui/LoadingSpinner';
-// import * as S from './style';
+import CardBasic from '../../ui/card/CardBasic';
+import { CardBasicSkeleton } from '../../ui/LoadingSkeleton';
+import LoadingSpinner from '../../ui/LoadingSpinner';
+import * as S from './style';
 
-// const CardContentList = ({ data = [], loading, lastElementRef }) => {
-//   return (
-//     <>
-//       <S.CardContentContainer>
-//         {loading && data.length === 0
-//           ? Array.from({ length: 10 }).map((_, index) => (
-//               <div key={index}>
-//                 <CardBasicSkeleton />
-//               </div>
-//             ))
-//           : data.map((item, index) => (
-//               <div key={item.id} ref={index === data.length - 1 ? lastElementRef : null}>
-//                 <CardBasic item={item} />
-//               </div>
-//             ))}
+const CardContentList = ({ data = [], loading, lastElementRef }) => {
+  return (
+    <>
+      <S.CardContentContainer>
+        {loading && data.length === 0
+          ? Array.from({ length: 10 }).map((_, index) => (
+              <div key={index}>
+                <CardBasicSkeleton />
+              </div>
+            ))
+          : data.map((item, index) => (
+              <div key={item.id} ref={index === data.length - 1 ? lastElementRef : null}>
+                <CardBasic item={item} />
+              </div>
+            ))}
 
-//         {loading && data.length > 0 && <LoadingSpinner />}
-//       </S.CardContentContainer>
-//     </>
-//   );
-// };
+        {loading && data.length > 0 && <LoadingSpinner />}
+      </S.CardContentContainer>
+    </>
+  );
+};
 
-// export default CardContentList;
+export default CardContentList;
